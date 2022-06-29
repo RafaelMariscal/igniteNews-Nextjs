ce4589beaf9cb9dd302e310114cea120afb0aa3c
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-06-29 19:01:43 -0300

    Feat/Adding faunaDB connection

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { fauna } from '../../../services/fauna'
+import { query as q } from 'faunadb'
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
 
 export default NextAuth({
   providers: [
@@ -8,4 +12,30 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email, name, following } = user;
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            {
+              data: {
+                email: email,
+                name: name,
+                following: following,
+              },
+            },
+          )
+        )
+          .then(obj => console.log('User created as :>>', obj))
+          .catch(err => console.error(err))
+
+        return true
+      } catch {
+        return false
+      }
+    },
+  }
 })
