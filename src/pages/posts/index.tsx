74c8ee7726dd96b9b28816822a6cb19d065e8f72
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-06-30 16:29:52 -0300

    Feat/Consuming primic documents data

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,22 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import Prismic from "@prismicio/client"
+import { getPrismicClient } from "../../services/prismic";
+import { RichText } from 'prismic-dom'
 import styles from './styles.module.scss'
 
-export default function Posts() {
+interface Post {
+  slug: string
+  title: string
+  excerpt: string
+  updatedAt: string
+}
+
+interface PostsPorps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsPorps) {
   return (
     <>
       <Head>
@@ -10,25 +25,48 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</strong>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum temporibus expedita aut quidem unde ipsum voluptates pariatur dolorum qui aliquid.</p>
-          </a>
-
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</strong>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum temporibus expedita aut quidem unde ipsum voluptates pariatur dolorum qui aliquid.</p>
-          </a>
-
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</strong>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum temporibus expedita aut quidem unde ipsum voluptates pariatur dolorum qui aliquid.</p>
-          </a>
+          {
+            posts.map(post => (
+              <a key={post.slug} href="#">
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            ))
+          }
         </div>
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+
+  const response = await prismic.query<any>([
+    Prismic.predicates.at('document.type', 'blogpost')
+  ], {
+    fetch: ['blogpost.title', 'blogpost.content'],
+    pageSize: 100,
+  }
+  )
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
